7f028b7b2fe5d88ccb228f23e3b196087716b993
VasilenkoYurii <yura.vasilenko98@gmail.com>
2023-02-16 19:08:40 +0200

    ready main tasks

---
+++ src/js/components/loadMoreBtn.js
@@ -0,0 +1,31 @@
+export default class LoadMoreBtn {
+  constructor({ selector, hidden = false }) {
+    this.refs = this.getRefs(selector);
+
+    hidden && this.hide();
+  }
+
+  getRefs(selector) {
+    const refs = {};
+    refs.button = document.querySelector(selector);
+    return refs;
+  }
+
+  enable() {
+    this.refs.button.disabled = false;
+    this.refs.button.textContent = 'Load more';
+  }
+
+  disable() {
+    this.refs.button.disabled = true;
+    this.refs.button.textContent = 'loading...';
+  }
+
+  show() {
+    this.refs.button.classList.remove('is-hidden');
+  }
+
+  hide() {
+    this.refs.button.classList.add('is-hidden');
+  }
+}
